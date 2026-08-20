80575cd53eb4f12b231d2576644796f9cc8201f6
kukla <kukla.1989@ukr.net>
2026-08-20 15:51:08 +0300

    initialize node js projectt with Express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/products/add', (req, res) => {
+  res.json({ message: 'initialize' });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
