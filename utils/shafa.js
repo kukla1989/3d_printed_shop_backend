bd74d0e8984003213c85f57daf0ed127d5d2c266
kukla <kukla.1989@ukr.net>
2026-08-31 11:21:06 +0300

    add fetchJsonLdFromUrl and fetchShafaData functions

---
+++ utils/shafa.js
@@ -0,0 +1,29 @@
+const cheerio = require("cheerio");
+
+async function fetchJsonLdFromUrl(url) {
+  const res = await fetch(url);
+  const html = await res.text();
+  const $ = cheerio.load(html);
+
+  return $('script[type="application/ld+json"]')
+    .map((_, script) => {
+      try {
+        return JSON.parse($(script).html());
+      } catch {
+        return null;
+      }
+    })
+    .get();
+}
+
+async function fetchShafaData(url) {
+  const data = await fetchJsonLdFromUrl(url)
+
+  const { name, image, description, color } = data[0]
+  const { price } = data[0].offers;
+
+  return { name, image, description, color, price };
+}
+
+
+fetchShafaData(url).then(console.log);
